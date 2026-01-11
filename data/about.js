da650e8b235839f472fd69a1aa26c54f4b550b1c
janfordP <127754896+janfordP@users.noreply.github.com>
2026-01-11 16:32:04 +1100

    Edit main page

--- data/about.js
+++ data/about.js
@@ -1,6 +1,6 @@
 export const about = {
-  name: "Janford P",
-  role: "Quality Assurance Engineer | AI & Automation",
+  //name: "Janford P",
+  role: "Civil Engineer | Quality Engineer | AI & Automation",
   summary: [
     "Quality Assurance Engineer in an Australian precast manufacturing company.",
     "Focused on building AI tools to improve quality of work and optimise engineering processes.",
